39575b78de215ab55c936dd39fcceaa91390a0f4
sudo-poggers <72994250+sudo-poggers@users.noreply.github.com>
2020-10-19 18:12:59 +0100

    Update and rename random numbers.py to random numbers (not working)

--- random numbers.py
+++
@@ -1,9 +0,0 @@
-import random
-randomnumber = random.randint(1,5)
-counter = 1
-number = int(input("Guess the number:")
-if number != randomnumber
-    input("incorrect")
-    counter = counter + 1
-print("correct, you had", counter, "attempts")
-    
