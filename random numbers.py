d9a8cdafcd8cd44a95d8315a24601b5cda6f0a5a
sudo-poggers <72994250+sudo-poggers@users.noreply.github.com>
2020-10-19 16:51:51 +0100

    random numbers
    19/10/20

---
+++ random numbers.py
@@ -0,0 +1,9 @@
+import random
+randomnumber = random.randint(1,5)
+counter = 1
+number = int(input("Guess the number:")
+if number != randomnumber
+    input("incorrect")
+    counter = counter + 1
+print("correct, you had", counter, "attempts")
+    
